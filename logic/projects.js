811de3761a9af5c2157cd0713f99a62676230e01
Mohamed Saad <131729886+mohamedsaadgit@users.noreply.github.com>
2023-12-31 19:55:28 +0200

    add new project

--- logic/projects.js
+++ logic/projects.js
@@ -1,32 +1,32 @@
 const project =[
     {
-        id:1,
-        img:"./public/project/project1.png",
-        path:"https://sneaker-react-app.netlify.app/"
+        img:"./public/project/project5.png",
+        path:"https://tradition-nice-one-client.vercel.app/"
     },
     {
-        id:2,
         img:"./public/project/project2.png",
         path:"https://plants-react-app.netlify.app/"
     },
     {
-        id:3,
+        img:"./public/project/project1.png",
+        path:"https://sneaker-react-app.netlify.app/"
+    },
+    {
         img:"./public/project/project3.png",
         path:"https://riva-react-app.netlify.app/"
     },
     {
-        id:4,
         img:"./public/project/project4.png",
         path:"https://plants-react-app.netlify.app/"
     },
 ]
 //start portfolo
 let container_portfolo = document.querySelector("#container-portfolo")
-project.map(item=>{
+project.map((item,index)=>{
     //start container
     let projects_card = document.createElement("div")
     projects_card.className = "projects-card"
-    projects_card.id = item.id
+    projects_card.id = index
     //start container img
     let projects_img = document.createElement("div")
     projects_img.className = "projects-img"
