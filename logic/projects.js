ceb36e783406491344548dba3658adc26fdbb877
Mohamed Saad <131729886+mohamedsaadgit@users.noreply.github.com>
2024-01-23 01:08:27 +0200

    update portfolo

--- logic/projects.js
+++ logic/projects.js
@@ -1,47 +1,59 @@
 const project =[
     {
         img:"./public/project/project5.png",
-        path:"https://tradition-nice-one-client.vercel.app/"
+        path:"https://imitation-nice-one-client.vercel.app",
+        alternate:"Imitation Nice One "
     },
     {
         img:"./public/project/project2.png",
-        path:"https://plants-react-app.netlify.app/"
-    },
-    {
-        img:"./public/project/project1.png",
-        path:"https://sneaker-react-app.netlify.app/"
+        path:"https://plants-react-app.netlify.app/",
+        alternate:"decoration plants"
     },
     {
         img:"./public/project/project3.png",
-        path:"https://riva-react-app.netlify.app/"
+        path:"https://riva-react-app.netlify.app/",
+        alternate:"Imitation Nice One "
+    },
+    {
+        img:"./public/project/project1.png",
+        path:"https://sneaker-react-app.netlify.app/",
+        alternate:"Snicker"
     },
     {
         img:"./public/project/project4.png",
-        path:"https://plants-react-app.netlify.app/"
+        path:"https://coffeeking-app.netlify.app/",
+        alternate:"Coffeeking"
     },
 ]
 //start portfolo
 let container_portfolo = document.querySelector("#container-portfolo")
-project.map((item,index)=>{
-    //start container
-    let projects_card = document.createElement("div")
-    projects_card.className = "projects-card"
-    projects_card.id = index
-    //start container img
-    let projects_img = document.createElement("div")
-    projects_img.className = "projects-img"
-    let img = document.createElement("img")
-    img.src = item.img
-    projects_img.appendChild(img)
-    projects_card.appendChild(projects_img)
-    //start linke
-    let rout_project = document.createElement("a")
-    rout_project.className = "rout-project"
-    rout_project.innerHTML= "Visit"
-    rout_project.href = item.path
-    rout_project.setAttribute("target", "_blank");
-    projects_card.appendChild(rout_project)
-    //start appund
-    container_portfolo.appendChild(projects_card)
+function displayProjects (projects) {
+    projects.forEach((item,index)=>{
+        let container =`
+        <div id=${index} class="projects-card" >
+            <div class ="projects-img">
+                <img src=${item.img} alt=${item.alternate}>
+            </div>
+            <a class="rout-project" href=${item.path} target="_blank">Visit</a>
+        </div>
+        `
+        container_portfolo.insertAdjacentHTML('beforeend', container);
+    })
+}
+displayProjects (project.slice(0,4))
+let btnredmor = document.querySelector(".btn-redmor")
+let statebtn = true
+btnredmor.addEventListener("click",()=>{
+    if(statebtn){
+        container_portfolo.innerHTML = "";
+        displayProjects (project)
+        btnredmor.innerHTML = "Offer less"
+        statebtn = false
+    }else{
+        container_portfolo.innerHTML = "";
+        displayProjects (project.slice(0,4))
+        btnredmor.innerHTML = "Show more"
+        statebtn = true
+    }
 })
 //end portfolo
